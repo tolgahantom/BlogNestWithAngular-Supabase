d4d7dca0ce303667286868c0d1f40e9436b1fe19
Tolgahan Tom <tomtolgahan@gmail.com>
2025-02-24 10:42:12 +0300

    yorum ekleme yapıldı

--- src/app/components/blog-detail-comments/blog-detail-comments.component.ts
+++ src/app/components/blog-detail-comments/blog-detail-comments.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { CommentService } from '../../services/comment.service';
+import { Observable } from 'rxjs';
+import { UserModel } from '../../models/user.model';
+import { AuthService } from '../../services/auth.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-blog-detail-comments',
@@ -8,16 +12,44 @@ import { CommentService } from '../../services/comment.service';
 })
 export class BlogDetailCommentsComponent implements OnInit {
   comments: any[] = [];
+  commentForm: FormGroup;
   @Input() id: any;
+  @Input() blogId: any;
+  userId: string = '';
 
-  constructor(private commentService: CommentService) {}
+  constructor(
+    private commentService: CommentService,
+    private authService: AuthService,
+    private fb: FormBuilder
+  ) {
+    this.authService.currentUser$.subscribe((data) => {
+      if (data) {
+        this.userId = data.id;
+      }
+    });
+    this.commentForm = this.fb.group({
+      content: [''],
+    });
+  }
 
   ngOnInit(): void {
     if (this.id) {
-      this.commentService.getComments(this.id).then((data) => {
-        this.comments = data;
-        console.log(data);
-      });
+      this.fetchComments();
     }
+  }
+
+  fetchComments() {
+    this.commentService.getComments(this.id).then((data) => {
+      this.comments = data;
+    });
+  }
+
+  addComment() {
+    this.commentService
+      .addComment(this.blogId, this.userId, this.commentForm.value.content)
+      .then((data) => {
+        this.fetchComments();
+        this.commentForm.reset();
+      });
   }
 }
